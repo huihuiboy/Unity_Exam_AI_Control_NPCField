9f2b3e6c034e2780e960fb12a5fff847a79ed5e0
huihuiboy <55218458+huihuiboy@users.noreply.github.com>
2019-11-07 11:25:00 +0800

    期中

---
+++ UnityProject/Assets/robot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robot : MonoBehaviour {
+
+    [Header("對話")]
+    public string Say = "腦袋裝鉛了嗎";
+    public float saySpeed = 0.2f;
+    [Header("任務")]
+    public bool Can = false;
+    public int bataryNum = 5;
+    public int batary = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ UnityProject/Assets/robot_ball.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robot_ball : MonoBehaviour {
+
+    [Header("速度"), Range(0, 100)]
+    public int Speed = 1;
+    [Header("跳躍"),Range(0f,1000f)]
+    public float Jump = 200f;
+    [Header("地板裝鉛了嗎")]
+    public bool IsGround = false;
+
+    private Rigidbody ball;
+
+    // Use this for initialization
+    void Start () {
+        ball = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.S)) Walk(0f,-0.1f);
+        if (Input.GetKey(KeyCode.W)) Walk(0f,0.1f);
+        if (Input.GetKey(KeyCode.A)) Walk(-0.1f,0f);
+        if (Input.GetKey(KeyCode.D)) Walk(0.1f,0f);
+        Jumpy();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        IsGround = true;
+    }
+    /// <summary>
+    /// 走起
+    /// </summary>
+    /// <param name="x">左右</param>
+    private void Walk(float x,float z)
+    {
+        this.transform.Translate(Speed * x, 0, Speed * z);
+    }
+    /// <summary>
+    /// 跳起來
+    /// </summary>
+    private void Jumpy()
+    {
+        if (Input.GetKey(KeyCode.Space) && IsGround == true)
+        {
+            IsGround = false;
+            GetComponent<Rigidbody>().AddForce(new Vector3(0f,Jump,0f));
+
+        }
+    }
+}
